68daa7398cbb5673c85d5e3dbfd2059463af83fd
Cuppixx <david.borgert@gmx.de>
2023-09-24 22:43:06 +0200

    Added temp Heightmap texture for testing. Improved upon the clipmap shader and scenes

--- scripts/clipmap.gd
+++ scripts/clipmap.gd
@@ -1,11 +1,45 @@
 extends StaticBody3D
 
+const TERRAIN:Resource = preload("res://scenes/clipmap_mesh.tscn")
+@onready var terrain:MeshInstance3D = $ClipmapMesh
+@onready var mesh_size:Vector2 = Vector2.ONE * terrain.mesh_square_size
 
-# Called when the node enters the scene tree for the first time.
+@onready var player:CharacterBody3D = $"../PlayerRegistry/Character"
+var player_position:Vector3 = Vector3.ZERO
+
+@export var endless_terrain:bool = false
+@export var grid_size:int = 0
+var timer = Timer.new()
+@export var update_interval_sec:float = 1.0
+
+const MESH_SQUARE_SIZE_DEFAULT:int = 512
+@export var mesh_square_size:int = MESH_SQUARE_SIZE_DEFAULT
+##Any additional entries in mesh_square_size_list must follow the same patter, I.e. [64,128,256,512,1024,2048].
+@export var mesh_square_size_list:Array = [8,16,32,64,128,256,512,1024,2048,4096]
+
 func _ready() -> void:
-	pass # Replace with function body.
+	for x in range(-grid_size,grid_size+1):
+		for z in range(-grid_size,grid_size+1):
+			if x==0 and z==0:
+				pass
+			else:
+				var terrain_partition:MeshInstance3D = TERRAIN.instantiate()
+				terrain_partition.x = x
+				terrain_partition.z = z
+				add_child(terrain_partition)
 
+	if endless_terrain == true:
+		add_child(timer)
+		timer.timeout.connect(update_position)
+		timer.set_wait_time(update_interval_sec)
+		update_position()
+	else:pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func update_position() -> void:
+	player_position = player.global_position.round() * Vector3(1,0,1)
+	self.global_position = player_position
+	terrain.material_override.set_shader_parameter("uv_x",player_position.x/mesh_size.x)
+	terrain.material_override.set_shader_parameter("uv_z",player_position.z/mesh_size.y)
+	terrain.material_override.set_shader_parameter("uv_x_frag",player_position.x/mesh_size.x)
+	terrain.material_override.set_shader_parameter("uv_z_frag",player_position.z/mesh_size.y)
+	timer.start()
--- scripts/clipmap_mesh.gd
+++ scripts/clipmap_mesh.gd
@@ -1,11 +1,47 @@
 extends MeshInstance3D
 
+var x:float
+var z:float
+var lod:int
+var subdivide_length:int
 
-# Called when the node enters the scene tree for the first time.
+@onready var parent_node:StaticBody3D = $".."
+var mesh_square_size:int
+var mesh_square_size_list:Array
+
+var texture_size:float = 48.0
+
 func _ready() -> void:
-	pass # Replace with function body.
+	mesh_square_size = parent_node.mesh_square_size
+	mesh_square_size_list = parent_node.mesh_square_size_list
 
+	var image_height:int = self.material_override.get_shader_parameter("heightmap").get_image().get_height()
+	#var image_height:int = self.material_override.get_shader_parameter("heightmap").get_height()
+	if mesh_square_size in mesh_square_size_list:
+		var image_index:int = mesh_square_size_list.find(image_height,0)
+		var mesh_index:int = mesh_square_size_list.find(mesh_square_size,0)
+		#print(image_index - mesh_index)
+		self.material_override.set_shader_parameter("heightmap_mipmap_lvl", (image_index - mesh_index))
+		#print("Mipmap", self.material_override.get_shader_parameter("heightmap_mipmap_lvl"))
+	else: mesh_square_size = image_height
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	self.mesh = QuadMesh.new()
+	self.mesh.orientation = mesh.FACE_Y
+	self.mesh.size = Vector2(mesh_square_size,mesh_square_size)
+
+	self.position = Vector3(x, 0, z) * mesh_square_size
+
+	lod = max(abs(x),abs(z))
+	subdivide_length = max((mesh_square_size / (pow(2.0, float(lod)))) -1, 0)
+	if lod == 0:
+		subdivide_length += mesh_square_size
+		#print(subdivide_length)
+	else: pass
+		#self.material_override.set_shader_parameter("Texture_UV_Scale", (texture_size/float(lod)))
+		#print("UV", self.material_override.get_shader_parameter("Texture_UV_Scale"))
+
+	self.mesh.subdivide_width = subdivide_length
+	self.mesh.subdivide_depth = subdivide_length
+
+	#print("LOD:", lod)
+	#print(subdivide_length)
