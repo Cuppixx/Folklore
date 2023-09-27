466dcb57c3a94844634e693fe3a53e6211643365
Cuppixx <david.borgert@gmx.de>
2023-09-27 11:04:29 +0200

    Cleaned up clipmap mesh related scripts and nodes

--- scripts/clipmap.gd
+++ scripts/clipmap.gd
@@ -10,19 +10,18 @@ var player_position:Vector3 = Vector3.ZERO
 @export var endless_terrain:bool = false
 @export var generate_lod:bool = false
 @export var grid_size:int = 0
-var timer = Timer.new()
+var timer:Timer
 @export var update_interval_sec:float = 1.0
 
 const MESH_SQUARE_SIZE_DEFAULT:int = 512
 @export var mesh_square_size:int = MESH_SQUARE_SIZE_DEFAULT
-##Any additional entries in mesh_square_size_list must follow the same patter, I.e. [64,128,256,512,1024,2048].
+##Any additional entries in mesh_square_size_list must follow the same patter. [br]Pattern: [...,128,256,512,1024,2048,...]
 @export var mesh_square_size_list:Array = [8,16,32,64,128,256,512,1024,2048,4096]
 
 func _ready() -> void:
 	for x in range(-grid_size,grid_size+1):
 		for z in range(-grid_size,grid_size+1):
-			if x==0 and z==0:
-				pass
+			if x==0 and z==0: pass
 			else:
 				var terrain_partition:MeshInstance3D = TERRAIN.instantiate()
 				terrain_partition.x = x
@@ -30,6 +29,7 @@ func _ready() -> void:
 				add_child(terrain_partition)
 
 	if endless_terrain == true:
+		timer = Timer.new()
 		add_child(timer)
 		timer.timeout.connect(update_position)
 		timer.set_wait_time(update_interval_sec)
--- scripts/clipmap_mesh.gd
+++ scripts/clipmap_mesh.gd
@@ -3,45 +3,42 @@ extends MeshInstance3D
 var x:float
 var z:float
 var lod:int
-var subdivide_length:int
+var subdivisions:int
 
 @onready var parent_node:StaticBody3D = $".."
 var mesh_square_size:int
 var mesh_square_size_list:Array
+var image_height:int
 
 var texture_size:float = 48.0
 
 func _ready() -> void:
+	image_height = self.material_override.get_shader_parameter("heightmap").get_image().get_height()
 	mesh_square_size = parent_node.mesh_square_size
 	mesh_square_size_list = parent_node.mesh_square_size_list
 
-	var image_height:int = self.material_override.get_shader_parameter("heightmap").get_image().get_height()
-	#var image_height:int = self.material_override.get_shader_parameter("heightmap").get_height()
-	if mesh_square_size in mesh_square_size_list:
+	if (mesh_square_size in mesh_square_size_list) and (image_height in mesh_square_size_list):
+		#Find the index of the ImageSize and the index of the MeshSize. Subtracting the MeshIndex from the
+		#ImageIndex gives the Value used by MipmapLevels.
 		var image_index:int = mesh_square_size_list.find(image_height,0)
 		var mesh_index:int = mesh_square_size_list.find(mesh_square_size,0)
-		#print(image_index - mesh_index)
 		self.material_override.set_shader_parameter("heightmap_mipmap_lvl", (image_index - mesh_index))
-		#print("Mipmap", self.material_override.get_shader_parameter("heightmap_mipmap_lvl"))
 	else: mesh_square_size = image_height
 
+	#Create a new QuadMesh
 	self.mesh = QuadMesh.new()
 	self.mesh.orientation = mesh.FACE_Y
 	self.mesh.size = Vector2(mesh_square_size,mesh_square_size)
 
+	#Set own position in Grid
 	self.position = Vector3(x, 0, z) * mesh_square_size
 
+	#Calculate LOD
 	lod = max(abs(x),abs(z))
-	subdivide_length = max((mesh_square_size / (pow(2.0, float(lod)))) -1, 0)
-	if lod == 0:
-		subdivide_length += mesh_square_size
-		#print(subdivide_length)
+	subdivisions = max((mesh_square_size / (pow(2.0, float(lod)))) -1, 0)
+	if lod == 0: subdivisions += mesh_square_size
 	else: pass
-		#self.material_override.set_shader_parameter("Texture_UV_Scale", (texture_size/float(lod)))
-		#print("UV", self.material_override.get_shader_parameter("Texture_UV_Scale"))
 
-	self.mesh.subdivide_width = subdivide_length
-	self.mesh.subdivide_depth = subdivide_length
-
-	#print("LOD:", lod)
-	#print(subdivide_length)
+	#Set subdivisions based on LOD calculations
+	self.mesh.subdivide_width = subdivisions
+	self.mesh.subdivide_depth = subdivisions
